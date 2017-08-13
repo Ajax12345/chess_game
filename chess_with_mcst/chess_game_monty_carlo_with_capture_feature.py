2b33db5e2be0b604df3108910d88f7f8830f570e
Ajax1234 <pentester18@gmail.com>
2017-08-13 15:00:00 -0400

    Update chess_game_monty_carlo_with_capture_feature.py

--- chess_with_mcst/chess_game_monty_carlo_with_capture_feature.py
+++ chess_with_mcst/chess_game_monty_carlo_with_capture_feature.py
@@ -6,6 +6,8 @@ import random
 import copy
 import time
 from collections import OrderedDict
+import string
+import re
 #TODO: Add move file output capability
 
 class ChessGame(chess_board_pieces.Board):
@@ -16,6 +18,9 @@ class ChessGame(chess_board_pieces.Board):
         #self.get_possible_moves()
         #self.play_small_game()
         self.piece_values = {"k":20, "q":10, "r":5, "p":1, "kn":3, "b":3}
+        self.current_moves = []
+        self.move_count = 1
+        self.material_score = [0, 0] #white, black
 
 
     def get_possible_moves(self):
@@ -94,7 +99,9 @@ class ChessGame(chess_board_pieces.Board):
                     #print "final_piece_to_move", final_piece_to_move
                     #print "opponents", opponents
                     #print "self.piece_values[final_piece_to_move[0]]", self.piece_values[final_piece_to_move[0]]
-                    current_piece_value = self.piece_values[final_piece_to_move[0]] if len(final_piece_to_move) != 4 else self.piece_values[i[:2]]
+                    #print "self.piece_values[i[:2]]", self.piece_values[final_piece_to_move[:1]]
+                    #current_piece_value = self.piece_values[final_piece_to_move[0]] if len(final_piece_to_move) != 4 else self.piece_values[i[:2]]
+                    current_piece_value = self.piece_values[final_piece_to_move[0]] if len(final_piece_to_move) != 4 else self.piece_values[final_piece_to_move[:2]]
 
                     if any(self.piece_values[i[0]] < current_piece_value if len(i) != 4 else 3 > current_piece_value for i in opponents):
                         #print "here"
@@ -113,10 +120,7 @@ class ChessGame(chess_board_pieces.Board):
                         self.move_piece(final_piece_to_move, best_possibilites[final_piece_to_move])
                         break
 
-                    '''
-                    self.move_piece(final_piece_to_move, best_possibilites[final_piece_to_move])
-                    break
-                    '''
+
 
 
         elif possibilites:
@@ -135,12 +139,36 @@ class ChessGame(chess_board_pieces.Board):
         piece = raw_input("Enter the piece name: ")
         self.move_piece(piece, (x-1, y-1))
 
+    def record_game(self, piece, location):
+        #number:[lowercase, UPPERCASE]
+        #(number, letter)
+        letters = {i:[a, string.ascii_uppercase[i]] for i, a in zip(range(8), string.ascii_lowercase[:8])}
+        #print "location", location
+        if piece[0] == "p":
+            self.current_moves.append(str(self.move_count)+"."+letters[location[1]][0]+str(location[0]+1))
+
+        else:
+            if len(piece) == 4:
+                self.current_moves.append(str(self.move_count)+"."+"N"+letters[location[1]][0]+str(location[0]+1))
+
+            else:
+                self.current_moves.append(str(self.move_count)+"."+piece[0].upper()+letters[location[1]][0]+str(location[0]+1))
+
+        self.move_count += 1
+
+
 
 
     def move_piece(self, piece, location):
+
+        self.record_game(piece, location)
+        print "self.current_moves", self.current_moves
         if "b" in piece and "w" not in piece: #black move
             a, b = location
             if "w" in self.view_board[a][b]:
+                the_enemy_piece = self.view_board[a][b]
+                the_enemy_piece_value = self.piece_values[the_enemy_piece[0]] if len(the_enemy_piece) != 4 else 3
+                self.material_score[1] += the_enemy_piece_value
 
                 white_piece = self.view_board[a][b]
                 try:
@@ -169,6 +197,9 @@ class ChessGame(chess_board_pieces.Board):
             a, b = location
             if "b" in self.view_board[a][b] and "w" not in self.view_board[a][b]:
                 #self.white_capture_rate += 1
+                the_enemy_piece = self.view_board[a][b]
+                the_enemy_piece_value = self.piece_values[the_enemy_piece[0]] if len(the_enemy_piece) != 4 else 3
+                self.material_score[0] += the_enemy_piece_value
                 black_piece = self.view_board[a][b]
                 try:
                     self.black_pieces.remove(black_piece)
@@ -215,6 +246,11 @@ class ChessGame(chess_board_pieces.Board):
                 if "b" in piece and "w" not in piece: #black move
                     a, b = location
                     if "w" in self.board_now[a][b]:
+                        '''
+                        the_enemy_piece = self.board_now[a][b]
+                        the_enemy_piece_value = self.piece_values[the_enemy_piece[0]] if len(the_enemy_piece) != 4 else 3
+                        self.material_score[1] += the_enemy_piece_value
+                        '''
                         self.black_capture_rate += 1
                         white_piece = self.board_now[a][b]
                         try:
@@ -334,25 +370,48 @@ if __name__ == "__main__":
     g = ChessGame()
 
     while True:
-        for i in range(8):
-            print "", i+1, "",
-        print
-        for row, i in enumerate(g.view_board):
-            for b in i:
-                if b == "-":
-                    print " "+b+" ",
-                else:
-                    print b,
-            print row+1
+        try:
+            for i in range(8):
+                print "", i+1, "",
             print
-        g.user_move()
-        #time.sleep(2)
-        for row, i in enumerate(g.view_board):
-            for b in i:
-                if b == "-":
-                    print " "+b+" ",
-                else:
-                    print b,
-            print row+1
-            print
-        g.get_possible_moves()
+            for row, i in enumerate(g.view_board):
+                for b in i:
+                    if b == "-":
+                        print " "+b+" ",
+                    else:
+                        print b,
+                print row+1
+                print
+            g.user_move()
+            #time.sleep(2)
+            for row, i in enumerate(g.view_board):
+                for b in i:
+                    if b == "-":
+                        print " "+b+" ",
+                    else:
+                        print b,
+                print row+1
+                print
+            g.get_possible_moves()
+        except KeyboardInterrupt:
+            break
+
+        
+    number_of_games = [int(i.strip('\n')) for i in open('game_numbers.txt')][0]
+    f = open('game_numbers.txt', 'w')
+    f.write(str(number_of_games + 1)+"\n")
+    f.close()
+    file_name = "chess game {}.txt".format(number_of_games)
+    new_file = open(file_name, 'a')
+
+    for i in g.current_moves:
+        new_file.write(i+"\n")
+
+    if g.material_score[0] > g.material_score[1]:
+        new_file.write("White wins, {}-{}\n".format(g.material_score[0], g.material_score[-1]))
+    elif g.material_score[0] < g.material_score[1]:
+        new_file.write("Black wins, {}-{}\n".format(g.material_score[-1], g.material_score[0]))
+    else:
+        new_file.write("Tie, {}-{}\n".format(g.material_score[-1], g.material_score[0]))
+    new_file.close()
+
